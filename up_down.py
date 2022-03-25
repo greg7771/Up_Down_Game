37931f47d9840b3c7a860b8f0b5b5d24ebc547c8
Odyssey <63042773+greg7771@users.noreply.github.com>
2022-03-25 11:33:38 -0400

    First Upload

---
+++ up_down.py
@@ -0,0 +1,51 @@
+import random
+
+num = random.randint(1,100)
+
+chance = 5
+
+ask = int(input("안녕 나는 구렁이야. 너랑 숫자게임을 하고시포! "))
+
+chance -= 1
+
+
+
+
+
+while chance >= 1 :
+    
+
+    if ask == num:
+        print("우와 맞췄어요! 대단해요",end="") # end="" 하면 줄바꿈이 없어진다
+        break
+
+    else:
+        print("다시 해보세요")
+        if ask < num:
+            print("참고로", ask , "보다 커요!")
+        
+        
+        else:
+            print("참고로 숫자는", ask , "보다 작아요!")
+        
+        ask = int(input("무엇인가요? "))
+        chance -= 1
+
+    if chance == 1:
+        print("기회는 단 한 번 뿐이니 신중해지세요!")
+
+
+    if chance == 0 :
+        print("땡 틀렸습니다~")
+        print("정답은", num, "입니다~")\
+
+
+
+
+
+
+
+
+# elif chance == 1:  # elif는 if와 else 사이에!
+#             print("숫자는", ask, "보다 작!")
+#             print("기회는 단 한 번 뿐이니 신중해지세요!")

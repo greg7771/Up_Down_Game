f1f40a07f4f26c43c94a35e4ea62b530d49ea841
Odyssey <63042773+greg7771@users.noreply.github.com>
2022-03-25 23:11:26 -0400

    Bug Fix Incompleted
    Fear when my code executed with no single error

--- up_down.py
+++ up_down.py
@@ -1,6 +1,8 @@
 import random
 
-num = random.randint(1,100)
+# num = random.randint(1,100)
+
+num = 10
 
 chance = 5
 
@@ -8,6 +10,10 @@ ask = int(input("안녕 나는 구렁이야. 너랑 숫자게임을 하고시포
 
 chance -= 1
 
+def correct():
+    if ask == num:
+            print("우와 맞췄어요! 대단해요",end="") # end="" 하면 줄바꿈이 없어진다
+            
 
 
 
@@ -15,29 +21,37 @@ chance -= 1
 while chance >= 1 :
     
 
-    if ask == num:
-        print("우와 맞췄어요! 대단해요",end="") # end="" 하면 줄바꿈이 없어진다
-        break
 
-    else:
-        print("다시 해보세요")
+        if ask == num:
+            correct()
+
+        else:
+                print("")
+                print("다시 해보세요")
+                print("") 
         if ask < num:
-            print("참고로", ask , "보다 커요!")
+                print("참고로", ask , "보다 커요!")
+                print("")
+                ask = int(input("무엇인가요? "))
+                chance -= 1
         
         
         else:
             print("참고로 숫자는", ask , "보다 작아요!")
+            print("")
+            ask = int(input("무엇인가요? "))
+            chance -= 1
         
-        ask = int(input("무엇인가요? "))
-        chance -= 1
+    
 
-    if chance == 1:
-        print("기회는 단 한 번 뿐이니 신중해지세요!")
+        if chance == 1:
+            print("기회는 단 한 번 뿐이니 신중해지세요!")
+             
 
-
-    if chance == 0 :
-        print("땡 틀렸습니다~")
-        print("정답은", num, "입니다~")\
+    
+        if chance == 0 :
+            print("땡 틀렸습니다~")
+            print("정답은", num, "입니다~")\
 
 
 

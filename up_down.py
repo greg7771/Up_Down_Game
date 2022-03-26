6f449d76e2d08d9be2d0944c80b279825f66a44e
greg7771 <thomas11020029@gmail.com>
2022-03-26 11:42:29 -0400

    Success

--- up_down.py
+++ up_down.py
@@ -29,20 +29,25 @@ while chance >= 1 :
                 print("참고로", ask , "보다 커요!")
                 print("")
                 ask = int(input("무엇인가요? "))
+                if ask == num:
+                    continue
                 chance -= 1        
         
         else:
             print("참고로 숫자는", ask , "보다 작아요!")
             print("")
             ask = int(input("무엇인가요? "))
+            if ask == num:
+                continue # while 문 처음으로
             chance -= 1
         
-        if chance == 0:
-            print("기회는 끝~")
-            break
         
         if chance == 1:
-            print("기회는 단 한 번 뿐이니 신중해지세요!"))
+            print("기회는 단 한 번 뿐이니 신중해지세요!")
+
+
+print("")
+print("정답은", num , "이였습니다~")
 
     
 
